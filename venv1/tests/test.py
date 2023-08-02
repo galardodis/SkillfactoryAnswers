1e86c381a38b5e828f0c3f97e3621106d8a8b210
galardodis <ilyla_dis@mail.ru>
2023-08-02 11:40:57 +0300

    new

--- venv1/tests/test.py
+++ venv1/tests/test.py
@@ -6,15 +6,24 @@ class TestCalc:
     def setup(self):
         self.calc = Calculator
 
+    def test_multiply(self):
+        assert self.calc.multiply(self, 5, 5) == 25
+
+    def test_dividion(self):
+        assert self.calc.division(self, 25, 5) == 5
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 9, 8) == 1
+
     def test_adding_success(self):
         assert self.calc.adding(self, 1, 1) == 2
-
-    def test_adding_unsuccsess(self):
-        assert self.calc.adding(self, 1, 1) == 3
 
     def test_zero_dividion(self):
         with pytest.raises(ZeroDivisionError):
             self.calc.division(self, 1, 0)
+
+    def test_adding_unsuccsess(self):
+        assert self.calc.adding(self, 1, 1) == 3
 
     def teardown(self):
         print('Выполнение метода Teardown')
