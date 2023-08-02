6b971a61f5f8466170509243856591e73d1a3dd3
galardodis <ilyla_dis@mail.ru>
2023-08-02 20:26:33 +0300

    new

---
+++ venv1/24.4/tests/api.py
@@ -0,0 +1,70 @@
+import requests
+
+
+class PetFriends:
+    def __init__(self):
+        self.base_url = 'https://petfriends.skillfactory.ru'
+
+    def get_api_key(self, email, password):
+        headers = {
+            'email': email,
+            'password': password
+        }
+        res = requests.get(f'{self.base_url}/api/key', headers=headers)
+        status = res.status_code
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
+    def get_list_of_pets(self, auth_key, filter):
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+
+        res = requests.get(f'{self.base_url}/api/pets', headers=headers, params=filter)
+        status = res.status_code
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
+    def add_information_about_new_pets(self, auth_key, name='', animal_type='', age=int):
+        headers = {'auth_key': auth_key['key']}
+        data = {
+            'name': name,
+            'animal_type': animal_type,
+            'age': age,
+        }
+        file = {'pet_photo': ('images/Cavalier-King-Charles-Spaniel_Puppy_Grass.jpg',
+                              open('images/Cavalier-King-Charles-Spaniel_Puppy_Grass.jpg', 'rb'), 'image/jpeg')}
+        res = requests.post(f'{self.base_url}/api/pets', headers=headers, data=data, files=file)
+        status = res.status_code
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
+    def delete_pet(self, auth_key, pet_id):
+        headers = {'auth_key': auth_key['key']}
+        res = requests.delete(f'{self.base_url}/api/pets/{pet_id}', headers=headers)
+        return res.status_code
+
+    def update_information_about_pet(self, auth_key, pet_id, name='', animal_type='', age=int):
+        headers = {'auth_key': auth_key['key']}
+        data = {
+            'name': name,
+            'animal_type': animal_type,
+            'age': age,
+        }
+        res = requests.put(f'{self.base_url}/api/pets/{pet_id}',
+                            headers=headers, data=data)
+        status = res.status_code
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
--- venv1/24.4/tests/test_pet_friends.py
+++ venv1/24.4/tests/test_pet_friends.py
@@ -16,3 +16,30 @@ def test_get_all_pets_whith_valid_key(filter=''):
     assert status == 200
     assert len(result['pets']) > 0
 
+def test_add_information_about_new_pets_valid_key():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_information_about_new_pets(auth_key, 'Нора', 'Кавалер-кинг-чарльз-спаниель', 1)
+    assert status == 200
+    assert result['name'] == 'Нора'
+    assert result['animal_type'] == 'Кавалер-кинг-чарльз-спаниель'
+    assert result['age'] == '1'
+    assert 'data:image/jpeg' in result['pet_photo']
+
+
+def test_deete_pet_valid_pet_id():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    pet_id = pf.get_list_of_pets(auth_key, filter='my_pets')[1]['pets'][0]['id']
+    status = pf.delete_pet(auth_key, pet_id)
+    assert status == 200
+
+def test_update_information_about_pet():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    pf.add_information_about_new_pets(auth_key, 'Нора', 'Кавалер-кинг-чарльз-спаниель', 1)
+    pet_id = pf.get_list_of_pets(auth_key, filter='my_pets')[1]['pets'][0]['id']
+    status, result = pf.update_information_about_pet(auth_key, pet_id,
+                                                     'Мира', 'Немецкая овчарка', 2)
+    assert status == 200
+    assert result['name'] == 'Мира'
+    assert result['animal_type'] == 'Немецкая овчарка'
+    assert result['age'] == '2'
+
